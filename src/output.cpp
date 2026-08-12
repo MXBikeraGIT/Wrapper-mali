15441209d00ad9f2d236ad1f59bcbdd7a21b57ad
qaqwa <mxbikeragit@gmail.com>
2026-08-12 09:34:02 +0800

    Update output.cpp

--- src/output.cpp
+++ src/output.cpp
@@ -1,67 +1,40 @@
 #include <vulkan/vulkan.h>
-#include <dlfcn.h>
-#include <android/log.h>
 
-#define LOG_TAG "AndroidOutput"
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
-#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+extern PFN_vkVoidFunction get_real_proc_addr(VkInstance instance, const char* pName);
 
-static void* g_real_libvulkan = nullptr;
-static PFN_vkGetInstanceProcAddr g_real_vkGetInstanceProcAddr = nullptr;
-static PFN_vkCreateShaderModule g_real_vkCreateShaderModule = nullptr;
-static PFN_vkCreateInstance g_real_vkCreateInstance = nullptr;
-
-void init_android_vulkan() {
-    if (g_real_libvulkan) return;
-
-    // Load native Android Bionic Vulkan driver directly from system
-    g_real_libvulkan = dlopen("/system/lib64/libvulkan.so", RTLD_NOW | RTLD_LOCAL);
-    if (!g_real_libvulkan) {
-        g_real_libvulkan = dlopen("libvulkan.so", RTLD_NOW | RTLD_LOCAL);
-    }
-
-    if (!g_real_libvulkan) {
-        LOGE("FATAL: Could not load Android system libvulkan.so!");
-        return;
-    }
-
-    g_real_vkGetInstanceProcAddr = reinterpret_cast<PFN_vkGetInstanceProcAddr>(
-        dlsym(g_real_libvulkan, "vkGetInstanceProcAddr")
-    );
-
-    LOGI("Successfully loaded system libvulkan.so dispatch table!");
-}
-
-PFN_vkVoidFunction get_real_proc_addr(VkInstance instance, const char* pName) {
-    if (!g_real_vkGetInstanceProcAddr) {
-        init_android_vulkan();
-    }
-    return g_real_vkGetInstanceProcAddr(instance, pName);
-}
-
-VkResult call_real_vkCreateShaderModule(
-    VkDevice device,
-    const VkShaderModuleCreateInfo* pCreateInfo,
-    const VkAllocationCallbacks* pAllocator,
-    VkShaderModule* pShaderModule) 
+VkResult call_real_vkEnumerateDeviceExtensionProperties(
+    VkPhysicalDevice physicalDevice,
+    const char* pLayerName,
+    uint32_t* pPropertyCount,
+    VkExtensionProperties* pProperties)
 {
-    if (!g_real_vkCreateShaderModule) {
-        g_real_vkCreateShaderModule = reinterpret_cast<PFN_vkCreateShaderModule>(
-            g_real_vkGetInstanceProcAddr(nullptr, "vkCreateShaderModule")
-        );
-    }
-    return g_real_vkCreateShaderModule(device, pCreateInfo, pAllocator, pShaderModule);
+    auto real_fn = reinterpret_cast<PFN_vkEnumerateDeviceExtensionProperties>(
+        get_real_proc_addr(nullptr, "vkEnumerateDeviceExtensionProperties"));
+    if (!real_fn) return VK_ERROR_INITIALIZATION_FAILED;
+    return real_fn(physicalDevice, pLayerName, pPropertyCount, pProperties);
 }
 
-VkResult call_real_vkCreateInstance(
-    const VkInstanceCreateInfo* pCreateInfo,
-    const VkAllocationCallbacks* pAllocator,
-    VkInstance* pInstance) 
+void call_real_vkGetPhysicalDeviceFeatures2(
+    VkPhysicalDevice physicalDevice,
+    VkPhysicalDeviceFeatures2* pFeatures)
 {
-    if (!g_real_vkCreateInstance) {
-        g_real_vkCreateInstance = reinterpret_cast<PFN_vkCreateInstance>(
-            g_real_vkGetInstanceProcAddr(nullptr, "vkCreateInstance")
-        );
+    auto real_fn = reinterpret_cast<PFN_vkGetPhysicalDeviceFeatures2>(
+        get_real_proc_addr(nullptr, "vkGetPhysicalDeviceFeatures2"));
+    if (!real_fn) {
+        real_fn = reinterpret_cast<PFN_vkGetPhysicalDeviceFeatures2>(
+            get_real_proc_addr(nullptr, "vkGetPhysicalDeviceFeatures2KHR"));
     }
-    return g_real_vkCreateInstance(pCreateInfo, pAllocator, pInstance);
+    if (real_fn) real_fn(physicalDevice, pFeatures);
+}
+
+VkResult call_real_vkCreateDevice(
+    VkPhysicalDevice physicalDevice,
+    const VkDeviceCreateInfo* pCreateInfo,
+    const VkAllocationCallbacks* pAllocator,
+    VkDevice* pDevice)
+{
+    auto real_fn = reinterpret_cast<PFN_vkCreateDevice>(
+        get_real_proc_addr(nullptr, "vkCreateDevice"));
+    if (!real_fn) return VK_ERROR_INITIALIZATION_FAILED;
+    return real_fn(physicalDevice, pCreateInfo, pAllocator, pDevice);
 }
