b3f05b49afdb91ae1b2e03698059944a2b5f69d4
qaqwa <mxbikeragit@gmail.com>
2026-08-12 18:55:34 +0800

    Update output.cpp

--- src/output.cpp
+++ src/output.cpp
@@ -12,12 +12,11 @@ void log_error(const std::string& message) {
     std::cerr << "[Bionic-Wrapper ERROR]: " << message << std::endl;
 }
 
-// Utility to dump modified SPIR-V binaries to disk for debugging in RenderDoc/SPIRV-Tools
 void dump_spirv_file(const std::string& filename, const std::vector<uint32_t>& code) {
     std::ofstream out(filename, std::ios::binary);
     if (out.is_open()) {
         out.write(reinterpret_cast<const char*>(code.data()), code.size() * sizeof(uint32_t));
         out.close();
-        log_info("Dumped shader SPIR-V to " + filename);
+        log_info("Dumped SPIR-V binary to " + filename);
     }
 }
