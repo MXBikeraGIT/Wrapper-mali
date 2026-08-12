28cdd05d3932947e0168a6f2633108ef65145518
qaqwa <mxbikeragit@gmail.com>
2026-08-12 09:29:08 +0800

    Update bridge.h

--- include/bridge.h
+++ include/bridge.h
@@ -2,20 +2,9 @@
 
 #include <vulkan/vulkan.h>
 
-// Intercepted Vulkan entry points
-VKAPI_ATTR VkResult VKAPI_CALL Hook_vkCreateShaderModule(
-    VkDevice device,
-    const VkShaderModuleCreateInfo* pCreateInfo,
-    const VkAllocationCallbacks* pAllocator,
-    VkShaderModule* pShaderModule);
-
-VKAPI_ATTR VkResult VKAPI_CALL Hook_vkCreateInstance(
-    const VkInstanceCreateInfo* pCreateInfo,
-    const VkAllocationCallbacks* pAllocator,
-    VkInstance* pInstance);
-
-// Exported ICD symbols for Winlator/DXVK loader
+// Exported Vulkan ICD entry points
 extern "C" {
     VKAPI_ATTR PFN_vkVoidFunction VKAPI_CALL vkGetInstanceProcAddr(VkInstance instance, const char* pName);
     VKAPI_ATTR PFN_vkVoidFunction VKAPI_CALL vk_icdGetInstanceProcAddr(VkInstance instance, const char* pName);
+    VKAPI_ATTR PFN_vkVoidFunction VKAPI_CALL vkGetDeviceProcAddr(VkDevice device, const char* pName);
 }
