a2e6e85c04a6552f971ce2ed8e63ead5d372d0dc
qaqwa <mxbikeragit@gmail.com>
2026-09-13 22:45:10 +0800

    Update logic.h

--- include/logic.h
+++ include/logic.h
@@ -2,29 +2,42 @@
 
 #include <vulkan/vulkan.h>
 
-VkResult logic_vkCreateShaderModule(
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Shader module interception and SPIR-V sanitization entry point (called by bridge.cpp)
+VkResult logic_process_spirv(
     VkDevice device,
     const VkShaderModuleCreateInfo* pCreateInfo,
     const VkAllocationCallbacks* pAllocator,
     VkShaderModule* pShaderModule);
 
+// Instance creation override
 VkResult logic_vkCreateInstance(
     const VkInstanceCreateInfo* pCreateInfo,
     const VkAllocationCallbacks* pAllocator,
     VkInstance* pInstance);
 
+// Device extension enumeration override
 VkResult logic_vkEnumerateDeviceExtensionProperties(
     VkPhysicalDevice physicalDevice,
     const char* pLayerName,
     uint32_t* pPropertyCount,
     VkExtensionProperties* pProperties);
 
+// Physical device feature queries override
 void logic_vkGetPhysicalDeviceFeatures2(
     VkPhysicalDevice physicalDevice,
     VkPhysicalDeviceFeatures2* pFeatures);
 
+// Logical device creation override
 VkResult logic_vkCreateDevice(
     VkPhysicalDevice physicalDevice,
     const VkDeviceCreateInfo* pCreateInfo,
     const VkAllocationCallbacks* pAllocator,
     VkDevice* pDevice);
+
+#ifdef __cplusplus
+}
+#endif
